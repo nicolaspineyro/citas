0f8f5055f387d833c8ab496f60b703fe006d1035
Nicolas Piñeyro <nicolaspineyro@users.noreply.github.com>
2020-12-09 16:01:31 -0300

    Added files

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+  Text
+} from 'react-native';
+
+const App = () => {
+  return (
+    <Text style={{textAlign: 'center'}}>Proyecto Citas</Text>
+  );
+};
+
+export default App;
